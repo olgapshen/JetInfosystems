dce5124d6c3b4b57fc4697dff8faa5c4259d5230
Olga Pshenichnikova <o.pshenichnikova@be-interactive.ru>
2018-02-15 13:03:29 +0300

    controllers

---
+++ src/main/java/ru/jetinfosystems/controllers/ContractController.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ru.jetinfosystems.controllers;
+
+import java.util.LinkedList;
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.jetinfosystems.models.Contract;
+
+/**
+ *
+ * @author Olga Pshenichnikova <o.pshenichnikova@be-interactive.ru>
+ */
+@RestController
+public class ContractController 
+{
+	@GetMapping("/contract/list")
+    public List<Contract> getDocuments() 
+	{
+		List<Contract> contracts = new LinkedList<>();
+		contracts.add(new Contract(0));
+		contracts.add(new Contract(1));
+		
+		return contracts;
+	}
+}
---
+++ src/main/java/ru/jetinfosystems/models/Contract.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ru.jetinfosystems.models;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ *
+ * @author Olga Pshenichnikova <o.pshenichnikova@be-interactive.ru>
+ */
+public class Contract 
+{
+	private final String name;
+
+	public Contract(int number) 
+	{
+		SimpleDateFormat df = new SimpleDateFormat("dd-MM-yy");
+		Date date = new Date();
+		name = String.format("%02d от %s", number, df.format(date));
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+}
